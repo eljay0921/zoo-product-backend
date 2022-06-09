845ffe41a350bd46908fa791f59e345bb88f6cb2
eljay_gh <lj7812@gmail.com>
2022-06-09 15:07:12 +0900

    Update sqlserver.constants.ts

--- src/common/database/constants/sqlserver.constants.ts
+++ src/common/database/constants/sqlserver.constants.ts
@@ -1,8 +1,8 @@
 // export const DB_PORT = 1433;
-export const MAIN_DB_HOST = '121.78.195.247';
-export const MAIN_DB_USER = 'esapi_servicerw01';
-export const MAIN_DB_PSWD = 'EsAPI1q2r@!';
+export const MAIN_DB_HOST = '**';
+export const MAIN_DB_USER = '**';
+export const MAIN_DB_PSWD = '**';
 
-export const PROD_DB_HOST = '121.78.195.33';
-export const PROD_DB_USER = 'esadmin06';
-export const PROD_DB_PSWD = '11es*admin!!';
+export const PROD_DB_HOST = '**';
+export const PROD_DB_USER = '**';
+export const PROD_DB_PSWD = '**';
